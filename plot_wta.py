882a8061d6b974583580ee3fa7b56188a2ff03dd
Andreas Hartel <ahartel@kip.uni-heidelberg.de>
2014-01-27 09:48:16 +0100

    fixed STDP parameters (esp. max weight), added colors and horizontal lines to plot,

--- plot_wta.py
+++ plot_wta.py
@@ -3,23 +3,41 @@ import matplotlib.pyplot as plt
 from wta import num
 
 file_stem = './Results/wta_np1_nest_'
-spikes = [None] * (num['inputs']+num['nodes']*num['exc_neurons'])
+spikes = [None] * (num['inputs']+num['nodes']*num['exc_neurons']+num['inh_neurons'])
 offset = num['inputs']
+
+color_input = 'red'
+color_exc = 'green'
+color_inh = 'blue'
 
 for i in range(num['inputs']):
 	spikes[i] = np.loadtxt(file_stem+'input_'+str(i)+'.ras',usecols=(0,))
-	plt.plot(spikes[i],np.ones(len(spikes[i]))*i,linestyle='',marker='|')
+	plt.plot(spikes[i],np.ones(len(spikes[i]))*i,linestyle='',marker='|',color=color_input)
+
+plt.axhline(y=offset)
 
 for i in range(num['nodes']):
 	for j in range(num['exc_neurons']):
 		spikes[offset+j] = []
 	#spikes.append(np.loadtxt(file_stem+'output_'+str(i)+'.ras',usecols=(0,),ndmin=1))
-	this_spikes = np.loadtxt(file_stem+'output_'+str(i)+'.ras',usecols=(0,1),ndmin=1)
+	this_spikes = np.loadtxt(file_stem+'output_'+str(i)+'.ras',usecols=(0,1),ndmin=2)
 	for spike in this_spikes:	
 		spikes[offset+int(spike[1])].append(spike[0])
 	for j in range(num['exc_neurons']):
-		plt.plot(spikes[offset+j],np.ones(len(spikes[offset+j]))*(offset+j),linestyle='',marker='|')
+		plt.plot(spikes[offset+j],np.ones(len(spikes[offset+j]))*(offset+j),linestyle='',marker='|',color=color_exc)
 
 	offset += num['exc_neurons']
+
+plt.axhline(y=offset)
+
+# inhibitory
+this_spikes = np.loadtxt(file_stem+'inhibit.ras',usecols=(0,1),ndmin=1)
+for j in range(num['inh_neurons']):
+	spikes[offset+j] = []
+for spike in this_spikes:
+	spikes[offset+int(spike[1])].append(spike[0])
+for j in range(num['inh_neurons']):
+	plt.plot(spikes[offset+j],np.ones(len(spikes[offset+j]))*(offset+j),linestyle='',marker='|',color=color_inh)
+
 		
 plt.show()
--- wta.py
+++ wta.py
@@ -32,22 +32,23 @@ exec("from pyNN.%s import *" % simulator_name)
 from pyNN.random import NumpyRNG
 
 seed = 764756387
-tstop = 1000.0 # ms
-input_rate = 300.0 # Hz
+tstop = 3000.0 # ms
+input_rate = 100.0 # Hz
 cell_params = {'tau_refrac': 2.0,  # ms
                'v_thresh':  -50.0, # mV
                'tau_syn_E':  2.0,  # ms
                'tau_syn_I':  2.0}  # ms
 
 num = {}
-num['nodes'] = 10
-num['exc_neurons'] = 20
-num['inh_neurons'] = 15
-num['inputs'] = 20
+num['nodes'] = 50
+num['neighbours'] = 4
+num['exc_neurons'] = 10
+num['inh_neurons'] = 30
+num['inputs'] = 200
 num['conns_per_input'] = 5
 
 if __name__ == "__main__":
-	node = setup(timestep=0.025, min_delay=1.0, max_delay=1.0, debug=True, quit_on_end=False)
+	node = setup(timestep=0.1, min_delay=1.0, max_delay=1.0, debug=True, quit_on_end=False)
 	print "Process with rank %d running on %s" % (node, socket.gethostname())
 
 
@@ -64,26 +65,31 @@ if __name__ == "__main__":
 		exc_populations.append(Population(num['exc_neurons'], IF_curr_exp, cell_params, label="exc"+str(i)))
 		exc_populations[i].record()
 
-	# 2) inhibitory populations
-	inh_populations = []
-	for i in range(num['nodes']):
-		inh_populations.append(Population(num['inh_neurons'], IF_curr_exp, cell_params, label="inh"+str(i)))
+	# 2) inhibitory population
+	inh_population = Population(num['inh_neurons'], IF_curr_exp, cell_params, label="inh")
+	inh_population.record()
 
 	# 3) connect exc. populations to neiboughring inh. population
 	exc_inh_projections = []
 	exc_exc_projections = []
-	#connector = OneToOneConnector(weights=1.0)
-	connector = FixedProbabilityConnector(0.5,weights=1.0)
+	inh_connector = FixedProbabilityConnector(0.6,weights=1.0)
 	for i in range(num['nodes']):
-		exc_inh_projections.append(Projection(exc_populations[i],inh_populations[i],connector))
-		exc_exc_projections.append(Projection(exc_populations[i],exc_populations[i],connector))
+		exc_inh_projections.append(Projection(exc_populations[i],inh_population,inh_connector))
+		for j in range(i-num['neighbours'],i+num['neighbours']+1):
+			if j != i:
+				exc_connector = OneToOneConnector(weights=1.0/abs(j-i))
+
+				if j<0:
+					j+=num['nodes']
+				if j> num['nodes']-1:
+					j-=num['nodes']
+
+				exc_exc_projections.append(Projection(exc_populations[i],exc_populations[j],exc_connector))
 
 	# 4) connect inh. populations to all other exc. populations
-	connector = FixedProbabilityConnector(0.5,weights=0.7)
+	connector = FixedProbabilityConnector(0.6,weights=-1.0)
 	for i in range(num['nodes']):
-		for j in range(num['nodes']):
-			if i != j:
-				Projection(inh_populations[i], exc_populations[j],connector)
+		Projection(inh_population, exc_populations[i],connector,target="inhibitory")
 
 	# 5)
 	input_populations = []
@@ -95,11 +101,11 @@ if __name__ == "__main__":
 	# 6)
 	stdp_model = STDPMechanism(
 		timing_dependence=SpikePairRule(tau_plus=20.0, tau_minus=20.0),
-		weight_dependence=AdditiveWeightDependence(w_min=0, w_max=0.02,
+		weight_dependence=AdditiveWeightDependence(w_min=0, w_max=1.0,
 		A_plus=0.01, A_minus=0.012)
 	)
 
-	connector = FixedProbabilityConnector(0.5, weights=2.0)
+	connector = FixedProbabilityConnector(0.7, weights=1.0)
 	for i in range(num['inputs']):
 		for j in range(num['nodes']):
 			projection = Projection(input_populations[i], exc_populations[j], connector, rng=rng, synapse_dynamics=SynapseDynamics(slow=stdp_model))
@@ -114,6 +120,7 @@ if __name__ == "__main__":
 	print "[%d] Writing spikes to disk" % node
 	for i in range(num['nodes']):
 		exc_populations[i].printSpikes('%s_output_%d.ras' % (file_stem,i))
+	inh_population.printSpikes('%s_inhibit.ras' % (file_stem,))
 	for i in range(num['inputs']):
 		input_populations[i].printSpikes('%s_input_%d.ras' % (file_stem,i))
 

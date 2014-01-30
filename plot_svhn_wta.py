366b629401e378b31eb83b947ad77528515722f3
Andreas Hartel <ahartel@kip.uni-heidelberg.de>
2014-01-30 13:25:03 +0100

    adde plotting and writing out of weights

--- plot_svhn_wta.py
+++ plot_svhn_wta.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pickle
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 from svhn_wta import num
 
 file_stem = './Results/svhn_wta_np1_nest_'
@@ -8,42 +10,64 @@ color_input = 'red'
 color_exc = 'green'
 color_inh = 'blue'
 
+skiprows = 0
+
 def plot_section(offset,num,file_leaf,color):
     spikes = [None] * num
     for j in range(num):
         spikes[j] = []
-    this_spikes = np.loadtxt(file_stem+file_leaf+'.ras',usecols=(0,1),ndmin=2)
+    this_spikes = np.loadtxt(file_stem+file_leaf+'.ras',usecols=(0,1),ndmin=2,skiprows=skiprows)
     for spike in this_spikes:
         spikes[int(spike[1])].append(spike[0])
     for j in range(num):
         plt.plot(spikes[j],np.ones(len(spikes[j]))*(offset+j),linestyle='',marker='|',color=color)
 
-offset = 0
-#plot_section(offset,num['inputs'],'input',color_input)
+if 0:
+    fig = plt.figure()
+    offset = 0
+    #plot_section(offset,num['inputs'],'input',color_input)
 
-offset = num['inputs']
-plt.axhline(y=offset)
-plot_section(offset,num['l0_exc_neurons'],'exc_0',color_exc)
+    offset = num['inputs']
+    plt.axhline(y=offset)
+    plot_section(offset,num['l0_exc_neurons'],'exc_0',color_exc)
 
-offset += num['l0_exc_neurons']
-plt.axhline(y=offset)
-plot_section(offset,num['l1_exc_neurons'],'exc_1',color_exc)
+    offset += num['l0_exc_neurons']
+    plt.axhline(y=offset)
+    plot_section(offset,num['l1_exc_neurons'],'exc_1',color_exc)
 
-offset += num['l1_exc_neurons']
-plt.axhline(y=offset)
-plot_section(offset,num['l0_inh_neurons'],'inh_0',color_inh)
+    offset += num['l1_exc_neurons']
+    plt.axhline(y=offset)
+    plot_section(offset,num['l0_inh_neurons'],'inh_0',color_inh)
 
-offset += num['l0_inh_neurons']
-plt.axhline(y=offset)
-plot_section(offset,num['l1_inh_neurons'],'inh_1',color_inh)
+    offset += num['l0_inh_neurons']
+    plt.axhline(y=offset)
+    plot_section(offset,num['l1_inh_neurons'],'inh_1',color_inh)
 
-for i in range(1,num['steps']):
-    plt.axvline(x=i*50,color='grey')
+    for i in range(1,num['steps']):
+        plt.axvline(x=i*50,color='grey')
 
-with open('%slabels.txt'%(file_stem,),'r') as f:
-    cnt = 0
-    for line in f.readlines(): 
-        plt.text(cnt*50+25,1000,line)
-        cnt += 1
+    with open('%slabels.txt'%(file_stem,),'r') as f:
+        cnt = 0
+        for line in f.readlines(): 
+            plt.text(cnt*50+25,1000,line)
+            cnt += 1
+
+# weights
+if 1:
+    fig = plt.figure()
+    (input_wgt,l1_wgt) = pickle.load(open("%sinitial_weights.wgt"%(file_stem,)))
+    plt.subplot(221)
+    plt.imshow(input_wgt,interpolation='nearest',aspect='auto')
+    plt.colorbar(orientation='vertical')
+    plt.subplot(223)
+    plt.imshow(l1_wgt,interpolation='nearest',aspect='auto')
+    plt.colorbar(orientation='vertical')
+    (input_wgt,l1_wgt) = pickle.load(open("%sfinal_weights.wgt"%(file_stem,)))
+    plt.subplot(222)
+    plt.imshow(input_wgt,interpolation='nearest',aspect='auto')
+    plt.colorbar(orientation='vertical')
+    plt.subplot(224)
+    plt.imshow(l1_wgt,interpolation='nearest',aspect='auto')
+    plt.colorbar(orientation='vertical')
 
 plt.show()
--- svhn_wta.py
+++ svhn_wta.py
@@ -1,13 +1,13 @@
 import numpy as np
 import scipy.io as sio
-import sys, socket, png
+import sys, socket, png, pickle
 
 from pyNN.utility import get_script_args
 from pyNN.random import NumpyRNG
 
 seed = 764756387
 tstop = 50.0 # ms
-input_rate = 100.0 # Hz
+input_rate = 50.0 # Hz
 cell_params = {'tau_refrac': 2.0,  # ms
                'v_thresh':  -50.0, # mV
                'tau_syn_E':  2.0,  # ms
@@ -16,13 +16,13 @@ cell_params = {'tau_refrac': 2.0,  # ms
 num = {}
 #num['nodes'] = 50
 #num['neighbours'] = 4
-num['l0_exc_neurons'] = 100
-num['l1_exc_neurons'] = 50
-num['l0_inh_neurons'] = 25
-num['l1_inh_neurons'] = 13
-num['inputs'] = 1024#*3
+num['l0_exc_neurons'] = 80
+num['l1_exc_neurons'] = 20
+num['l0_inh_neurons'] = 20
+num['l1_inh_neurons'] = 5
+num['inputs'] = 1024*3
 #num['conns_per_input'] = 5
-num['steps'] = 2000
+num['steps'] = 5000
 
 basepath = '/home/ahartel/data/SVHN/train'
 
@@ -40,15 +40,15 @@ def setupNetwork():
     print "[%d] Creating populations" % node
     # 1) excitatory populations
     l0_exc_population = pynn.Population(num['l0_exc_neurons'], pynn.IF_curr_exp, cell_params, label="exc0")
-    l0_exc_population.record()
+    #l0_exc_population.record()
     l1_exc_population = pynn.Population(num['l1_exc_neurons'], pynn.IF_curr_exp, cell_params, label="exc1")
-    l1_exc_population.record()
+    #l1_exc_population.record()
 
     # 2) inhibitory population
     l0_inh_population = pynn.Population(num['l0_inh_neurons'], pynn.IF_curr_exp, cell_params, label="inh0")
-    l0_inh_population.record()
+    #l0_inh_population.record()
     l1_inh_population = pynn.Population(num['l1_inh_neurons'], pynn.IF_curr_exp, cell_params, label="inh1")
-    l1_inh_population.record()
+    #l1_inh_population.record()
 
     # 3) connect exc. populations to neiboughring inh. population
     inh_connector = pynn.FixedProbabilityConnector(0.6,weights=1.0)
@@ -73,7 +73,7 @@ def setupNetwork():
     #            exc_exc_projections.append(Projection(exc_populations[i],exc_populations[j],exc_connector))
 
     # 4) connect inh. populations to exc. populations
-    connector = pynn.FixedProbabilityConnector(0.6,weights=-1.0)
+    connector = pynn.FixedProbabilityConnector(0.6,weights=-1.3)
     l0_inh_exc_projection = pynn.Projection(l0_inh_population, l0_exc_population,connector,target="inhibitory")
     l1_inh_exc_projection = pynn.Projection(l1_inh_population, l1_exc_population,connector,target="inhibitory")
 
@@ -88,12 +88,12 @@ def setupNetwork():
         A_plus=0.01, A_minus=0.012)
     )
 
-    connector = pynn.FixedProbabilityConnector(0.6, weights=1.0)
+    connector = pynn.AllToAllConnector(weights=pynn.RandomDistribution(distribution='uniform',parameters=[0.3,1.0],rng=rng))
     input_projection = pynn.Projection(input_population, l0_exc_population, connector, rng=rng, synapse_dynamics=pynn.SynapseDynamics(slow=stdp_model))
-    connector = pynn.FixedProbabilityConnector(0.6, weights=1.0)
+    #connector = pynn.FixedProbabilityConnector(0.6, weights=1.0)
     l1_projection = pynn.Projection(l0_exc_population, l1_exc_population, connector, rng=rng, synapse_dynamics=pynn.SynapseDynamics(slow=stdp_model))
 
-    return node,l0_exc_population,l1_exc_population,l0_inh_population,l1_inh_population,input_population
+    return node,l0_exc_population,l1_exc_population,l0_inh_population,l1_inh_population,input_population,input_projection,l1_projection
 
 if __name__ == "__main__":
     simulator_name = 'nest'
@@ -103,10 +103,14 @@ if __name__ == "__main__":
         print "Using default simulator nest"
     exec("import pyNN.%s as pynn" % simulator_name)
 
-    node,l0_exc_population,l1_exc_population,l0_inh_population,l1_inh_population,input_population = setupNetwork()
+    node,l0_exc_population,l1_exc_population,l0_inh_population,l1_inh_population,input_population,input_projection,l1_projection = setupNetwork()
 
     file_stem = "Results/svhn_wta_np%d_%s" % (pynn.num_processes(), simulator_name)
     #projection.saveConnections('%s.conn' % file_stem)
+
+    # save initial weights
+    with open("%s_initial_weights.wgt"%(file_stem,),'wb') as f:
+        pickle.dump([input_projection.getWeights(format='array'),l1_projection.getWeights(format='array')],f)
 
     # input population views
     input_pop_views = []
@@ -119,7 +123,6 @@ if __name__ == "__main__":
     stop=num['steps']
     for i in range(len(mat['y'])):
         stop-=1
-        print stop
         if stop==0:
             break
         cnt = 0
@@ -128,26 +131,30 @@ if __name__ == "__main__":
         for row in data[:,:,:,i]:
             data_set = ()
             for pixel in row:
-                total=0
+                #total=0
                 for value in pixel:
                     data_set += (value,)
-                    total+=value
-                input_pop_views[cnt].set('rate',100.0/(3*256)*total)
-                cnt+=1
+                    #total+=value
+                    input_pop_views[cnt].set('rate',input_rate/(256)*value)
+                    cnt+=1
             png_data.append(data_set)
 
         with open(basepath+'/generated/'+str(i)+'.png','wb') as f:
             w = png.Writer(32,32)
             w.write(f,png_data)
 
-        print "[%d] Running simulation" % node
+        print "[%d] Running simulation step %d" % (node,stop)
         pynn.run(tstop)
 
+    # save final weights
+    with open("%s_final_weights.wgt"%(file_stem,),'wb') as f:
+        pickle.dump([input_projection.getWeights(format='array'),l1_projection.getWeights(format='array')],f)
+
     print "[%d] Writing spikes to disk" % node
-    l0_exc_population.printSpikes('%s_exc_0.ras' % (file_stem,))
-    l1_exc_population.printSpikes('%s_exc_1.ras' % (file_stem,))
-    l0_inh_population.printSpikes('%s_inh_0.ras' % (file_stem,))
-    l1_inh_population.printSpikes('%s_inh_1.ras' % (file_stem,))
+    #l0_exc_population.printSpikes('%s_exc_0.ras' % (file_stem,))
+    #l1_exc_population.printSpikes('%s_exc_1.ras' % (file_stem,))
+    #l0_inh_population.printSpikes('%s_inh_0.ras' % (file_stem,))
+    #l1_inh_population.printSpikes('%s_inh_1.ras' % (file_stem,))
     #input_population.printSpikes('%s_input.ras' % (file_stem,))
     with open('%s_labels.txt'%(file_stem,),'w') as f:
         for value in mat['y'][0:num['steps']-1]:

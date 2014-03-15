61cb40d899de184e4d9b236887372b7576f3bfbe
Andreas Hartel <ahartel@kip.uni-heidelberg.de>
2014-03-15 22:01:22 +0100

    latest state of non-ring svhn wta

--- plot_svhn_wta.py
+++ plot_svhn_wta.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-from svhn_wta import num
+from svhn_wta import num,tstop
 
 file_stem = './Results/svhn_wta_np1_nest_'
 
@@ -10,48 +10,79 @@ color_input = 'red'
 color_exc = 'green'
 color_inh = 'blue'
 
-skiprows = 0
+#skiprows = 100000
+plot_rates = True
 
-def plot_section(offset,num,file_leaf,color):
-    spikes = [None] * num
-    for j in range(num):
+def plot_section(ax1,offset,num_neurons,file_leaf,color,plot_rates,skiprows):
+    ax1.set_xlabel('time [ms]')
+    ax1.set_ylabel('neuron number')
+    # prepare empty spike arrays
+    spikes = [None] * num_neurons
+    for j in range(num_neurons):
         spikes[j] = []
+    # load spikes from file
     this_spikes = np.loadtxt(file_stem+file_leaf+'.ras',usecols=(0,1),ndmin=2,skiprows=skiprows)
+    # create coarser time bins
+    bins = np.arange(num['steps'])*tstop
+    large_bins = np.arange(5+1)*1000
+    rates = np.zeros(num['steps'])
+    #second_rates = np.zeros(5)
+    bin_count = 0
+    #large_bin_count = 0
     for spike in this_spikes:
         spikes[int(spike[1])].append(spike[0])
-    for j in range(num):
-        plt.plot(spikes[j],np.ones(len(spikes[j]))*(offset+j),linestyle='',marker='|',color=color)
+        try:
+            while spike[0] > bins[bin_count+1]:
+                bin_count += 1
+        except IndexError:
+            pass
+        rates[bin_count] += 1
 
-if 1:
-    fig = plt.figure()
-    offset = 0
-    #plot_section(offset,num['inputs'],'input',color_input)
+        #while spike[0] > large_bins[large_bin_count+1]:
+        #    large_bin_count += 1
+        #second_rates[large_bin_count] += 1
 
-    offset = num['inputs']
-    plt.axhline(y=offset)
-    plot_section(offset,num['l0_exc_neurons'],'exc_0',color_exc)
+    rates /= num_neurons*tstop/1000
+    #second_rates /= num_neurons
 
-    offset += num['l0_exc_neurons']
-    plt.axhline(y=offset)
-    plot_section(offset,num['l1_exc_neurons'],'exc_1',color_exc)
+    for j in range(num_neurons):
+        ax1.plot(spikes[j],np.ones(len(spikes[j]))*(offset+j),linestyle='',marker='|',color=color)
 
-    offset += num['l1_exc_neurons']
-    plt.axhline(y=offset)
-    plot_section(offset,num['l0_inh_neurons'],'inh_0',color_inh)
+    ax2 = ax1.twinx()
+    ax2.plot(bins,rates)
+    ax2.set_ylabel('firing rate [Hz]')
+    #ax2.plot(large_bins[0:5],second_rates)
 
-    offset += num['l0_inh_neurons']
-    plt.axhline(y=offset)
-    plot_section(offset,num['l1_inh_neurons'],'inh_1',color_inh)
-    offset += num['l1_inh_neurons']
-
+def plot_input_numbers():
     for i in range(1,num['steps']):
         plt.axvline(x=i*50,color='grey')
 
     with open('%slabels.txt'%(file_stem,),'r') as f:
         cnt = 0
         for line in f.readlines(): 
-            plt.text(cnt*50+25,offset,line)
+            plt.text(cnt*50+25,10,line)
             cnt += 1
+
+if 1:
+    fig = plt.figure()
+    offset = 0
+    ax0 = plt.subplot(511)
+    plot_section(ax0,offset,num['inputs'],'input',color_input, plot_rates, 0)
+
+    ax1 = plt.subplot(512,sharex=ax0)
+    plot_section(ax1,offset,num['l0_exc_neurons'],'exc_0',color_exc, plot_rates, 0)
+    plot_input_numbers()
+
+    ax2 = plt.subplot(513,sharex=ax0)
+    plot_section(ax2,offset,num['l0_inh_neurons'],'inh_0',color_inh, plot_rates, 0)
+
+    ax3 = plt.subplot(514,sharex=ax0)
+    plot_section(ax3,offset,num['l1_inh_neurons'],'inh_1',color_inh, plot_rates, 0)
+
+    ax4 = plt.subplot(515,sharex=ax0)
+    plot_section(ax4,offset,num['l1_exc_neurons'],'exc_1',color_exc, plot_rates, 0)
+
+
 
 # weights
 if 1:
--- svhn_wta.py
+++ svhn_wta.py
@@ -7,22 +7,43 @@ from pyNN.random import NumpyRNG
 
 seed = 764756387
 tstop = 50.0 # ms
-input_rate = 50.0 # Hz
-cell_params = {'tau_refrac': 2.0,  # ms
-               'v_thresh':  -50.0, # mV
-               'tau_syn_E':  2.0,  # ms
-               'tau_syn_I':  2.0}  # ms
+input_rate = 40.0 # Hz
+cell_params = {'tau_refrac':  2.0,  # ms
+               'v_thresh'  :-50.0,  # mV
+               'tau_m'     : 10.0,  # ms 
+               'tau_syn_E' :  2.5,  # ms
+               'tau_syn_I' :  5.0}  # ms
 
 num = {}
 #num['nodes'] = 50
 #num['neighbours'] = 4
 num['l0_exc_neurons'] = 50
-num['l1_exc_neurons'] = 16
-num['l0_inh_neurons'] = 10
+num['l1_exc_neurons'] = 10
+num['l0_inh_neurons'] = 16
 num['l1_inh_neurons'] = 3
 num['inputs'] = 1024*3
-#num['conns_per_input'] = 5
-num['steps'] = 100
+num['steps'] = 50
+
+#p_inp_exc0 = 0.01
+#w_inp_exc0 = 0.00001
+
+w_inp_exc0_max = 0.0004
+w_exc0_exc1_max = 0.003
+
+p_exc0_inh0 = 0.6
+w_exc0_inh0 = 0.005
+
+p_exc0_exc0 = 0.5
+w_exc0_exc0 = 0.001
+
+p_exc1_inh1 = 1.0
+w_exc1_inh1 = 0.02
+
+p_inh0_exc0 = 0.6
+w_inh0_exc0 = 0.01
+
+p_inh1_exc1 = 1.0
+w_inh1_exc1 = 0.1
 
 basepath = '/home/ahartel/data/SVHN/train'
 
@@ -30,33 +51,35 @@ def setupNetwork():
     node = pynn.setup(timestep=0.1, min_delay=1.0, max_delay=1.0, debug=True, quit_on_end=False)
     print "Process with rank %d running on %s" % (node, socket.gethostname())
 
-
     rng = NumpyRNG(seed=seed, parallel_safe=True)
 
-    #n_spikes = int(2*tstop*input_rate/1000.0)
-    #spike_times = numpy.add.accumulate(rng.next(n_spikes, 'exponential',
-    #                       [1000.0/input_rate], mask_local=False))
-
     print "[%d] Creating populations" % node
-    # 1) excitatory populations
-    l0_exc_population = pynn.Population(num['l0_exc_neurons'], pynn.IF_cond_alpha, cell_params, label="exc0")
+    # 1) create excitatory populations
+    l0_exc_population = pynn.Population(num['l0_exc_neurons'], pynn.IF_cond_exp, cell_params, label="exc0")
     l0_exc_population.record()
-    l1_exc_population = pynn.Population(num['l1_exc_neurons'], pynn.IF_cond_alpha, cell_params, label="exc1")
+    #l0_exc_population.record_v()
+    l1_exc_population = pynn.Population(num['l1_exc_neurons'], pynn.IF_cond_exp, cell_params, label="exc1")
     l1_exc_population.record()
 
-    # 2) inhibitory population
-    l0_inh_population = pynn.Population(num['l0_inh_neurons'], pynn.IF_cond_alpha, cell_params, label="inh0")
+    # 2) create inhibitory population
+    l0_inh_population = pynn.Population(num['l0_inh_neurons'], pynn.IF_cond_exp, cell_params, label="inh0")
     l0_inh_population.record()
-    l1_inh_population = pynn.Population(num['l1_inh_neurons'], pynn.IF_cond_alpha, cell_params, label="inh1")
+    l1_inh_population = pynn.Population(num['l1_inh_neurons'], pynn.IF_cond_exp, cell_params, label="inh1")
     l1_inh_population.record()
 
-    # 3) connect exc. populations to neiboughring inh. population
-    inh_connector = pynn.FixedProbabilityConnector(0.6,weights=0.007)
+    # 3) exc0 -> inh0
+    inh_connector = pynn.FixedProbabilityConnector(p_exc0_inh0,weights=w_exc0_inh0)
     l0_exc_inh_projection = pynn.Projection(l0_exc_population,l0_inh_population,inh_connector)
+
+    # 4) exc1 -> inh1
+    inh_connector = pynn.FixedProbabilityConnector(p_exc1_inh1,weights=w_exc1_inh1)
     l1_exc_inh_projection = pynn.Projection(l1_exc_population,l1_inh_population,inh_connector)
 
-    exc_connector = pynn.FixedProbabilityConnector(0.6,weights=0.01,allow_self_connections=False)
-    #l0_exc_exc_projection = pynn.Projection(l0_exc_population,l0_exc_population,exc_connector)
+    # 5) exc0 -> exc0
+    exc_connector = pynn.FixedProbabilityConnector(p_exc0_exc0,weights=w_exc0_exc0,allow_self_connections=False)
+    l0_exc_exc_projection = pynn.Projection(l0_exc_population,l0_exc_population,exc_connector)
+
+    # 6) exc1 -> exc1
     #l1_exc_exc_projection = pynn.Projection(l1_exc_population,l1_exc_population,exc_connector)
 
     #for i in range(num['nodes']):
@@ -72,30 +95,85 @@ def setupNetwork():
 
     #            exc_exc_projections.append(Projection(exc_populations[i],exc_populations[j],exc_connector))
 
-    # 4) connect inh. populations to exc. populations
-    connector = pynn.FixedProbabilityConnector(0.6,weights=0.024)
-    l0_inh_exc_projection = pynn.Projection(l0_inh_population, l0_exc_population,connector,target="inhibitory")
-    l1_inh_exc_projection = pynn.Projection(l1_inh_population, l1_exc_population,connector,target="inhibitory")
+    # 7) inh0 -> exc0
+    connector = pynn.FixedProbabilityConnector(p_inh0_exc0,weights=w_inh0_exc0)
+    l0_inh_exc_projection = pynn.Projection(
+            l0_inh_population,
+            l0_exc_population,
+            connector,
+            target="inhibitory"
+        )
 
-    # 5)
-    input_population = pynn.Population(num['inputs'], pynn.SpikeSourcePoisson, {'rate': input_rate }, label="input")
+    # 8) inh1 -> exc1
+    connector = pynn.FixedProbabilityConnector(p_inh1_exc1,weights=w_inh1_exc1)
+    l1_inh_exc_projection = pynn.Projection(
+            l1_inh_population,
+            l1_exc_population,
+            connector,
+            target="inhibitory"
+        )
+
+    # 9) create input population
+    input_population = pynn.Population(
+            num['inputs'],
+            pynn.SpikeSourcePoisson,
+            {
+                'rate': input_rate
+            },
+            label="input"
+        )
     input_population.record()
 
-    # 6)
+    # 10) input -> exc0
     stdp_model = pynn.STDPMechanism(
         timing_dependence=pynn.SpikePairRule(tau_plus=20.0, tau_minus=20.0),
-        weight_dependence=pynn.AdditiveWeightDependence(w_min=0, w_max=1.0,
+        weight_dependence=pynn.AdditiveWeightDependence(w_min=0, w_max=w_inp_exc0_max,
         A_plus=0.01, A_minus=0.012)
     )
+    connector = pynn.AllToAllConnector(
+            weights=pynn.RandomDistribution(
+                distribution='uniform',
+                parameters=[0.00,w_inp_exc0_max],
+                rng=rng
+            )
+        )
+    #connector = pynn.FixedProbabilityConnector(p_inp_exc0,weights=w_inp_exc0)
+    input_projection = pynn.Projection(
+            input_population,
+            l0_exc_population,
+            connector,
+            rng=rng,
+            #synapse_dynamics=pynn.SynapseDynamics(slow=stdp_model)
+        )
 
-#    connector = pynn.AllToAllConnector(weights=pynn.RandomDistribution(distribution='uniform',parameters=[0.005,0.01],rng=rng))
-    connector = pynn.FixedProbabilityConnector(0.01,weights=0.01)
-    input_projection = pynn.Projection(input_population, l0_exc_population, connector, rng=rng, synapse_dynamics=pynn.SynapseDynamics(slow=stdp_model))
-    connector = pynn.FixedProbabilityConnector(0.05, weights=0.01)
-    l1_projection = pynn.Projection(l0_exc_population, l1_exc_population, connector, rng=rng, synapse_dynamics=pynn.SynapseDynamics(slow=stdp_model))
+    # 11) exc0 -> exc1
+    stdp_model = pynn.STDPMechanism(
+        timing_dependence=pynn.SpikePairRule(tau_plus=20.0, tau_minus=20.0),
+        weight_dependence=pynn.AdditiveWeightDependence(w_min=0, w_max=w_exc0_exc1_max,
+        A_plus=0.01, A_minus=0.012)
+    )
+    connector = pynn.AllToAllConnector(
+            weights=pynn.RandomDistribution(
+                distribution='uniform',
+                parameters=[0.00,w_exc0_exc1_max],
+                rng=rng
+            )
+        )
+    #connector = pynn.FixedProbabilityConnector(0.05, weights=0.01)
+    l1_projection = pynn.Projection(
+            l0_exc_population,
+            l1_exc_population,
+            connector,
+            rng=rng,
+            #synapse_dynamics=pynn.SynapseDynamics(slow=stdp_model)
+        )
 
     return node,l0_exc_population,l1_exc_population,l0_inh_population,l1_inh_population,input_population,input_projection,l1_projection
 
+
+#########
+# main
+#########
 if __name__ == "__main__":
     simulator_name = 'nest'
     try:
@@ -110,13 +188,17 @@ if __name__ == "__main__":
     #projection.saveConnections('%s.conn' % file_stem)
 
     # save initial weights
+    input_weights = input_projection.getWeights(format='array')
+    l1_weights = l1_projection.getWeights(format='array')
     with open("%s_initial_weights.wgt"%(file_stem,),'wb') as f:
-        pickle.dump([input_projection.getWeights(format='array'),l1_projection.getWeights(format='array')],f)
+        pickle.dump([input_weights,l1_weights],f)
 
     # input population views
     input_pop_views = []
     for i in range(num['inputs']):
         input_pop_views.append(pynn.PopulationView(input_population,[i]))
+
+    afferents = [0.0 for i in range(num['inputs'])]
 
     # read matlab file with image data and generate input firing rates
     mat = sio.loadmat(basepath+'/train_32x32.mat')
@@ -137,9 +219,11 @@ if __name__ == "__main__":
                     data_set += (value,)
                     #total+=value
                     input_pop_views[cnt].set('rate',input_rate/(256)*value)
+                    #print input_weights[cnt]
                     cnt+=1
             png_data.append(data_set)
 
+        # write out a png for debugging
         with open(basepath+'/generated/'+str(i)+'.png','wb') as f:
             w = png.Writer(32,32)
             w.write(f,png_data)
@@ -160,6 +244,8 @@ if __name__ == "__main__":
     with open('%s_labels.txt'%(file_stem,),'w') as f:
         for value in mat['y'][0:num['steps']-1]:
             f.write('%s\n'%(value[0]))
+
+    #l0_exc_population.print_v('%s_exc_0.v' % (file_stem,),compatible_output=False)
 
     print "[%d] Finishing" % node
     pynn.end()

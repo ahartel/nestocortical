8de7e93c99ab5f1aaa6a0f951d57bc2380dda762
Andreas Hartel <ahartel@kip.uni-heidelberg.de>
2014-06-16 10:25:58 +0200

    SEM input ring WTA in Nest with input

--- SEM_ring_wta_nopynn.py
+++ SEM_ring_wta_nopynn.py
@@ -1,34 +1,15 @@
+import sys, copy
 from nest import *
 import nest.topology as tp
-import SEM_input
 import nest.voltage_trace as vt
 import nest.raster_plot as rp
 import matplotlib.pyplot as plt
 
-#t_sim = 10000.0
-#n_ex = 16000
-#n_in = 4000
-#r_ex = 5.0
-#r_in = 12.5
-#epsc = 45.0
-#ipsc = -45.0
-#
-#neuron = Create("aeif_cond_exp")
-#noise = Create("poisson_generator",2)
-#voltmeter = Create("voltmeter")
-#spikedetector = Create("spike_detector")
-#
-#SetStatus(noise, [{"rate": n_ex*r_ex}, {"rate": n_in*r_in}])
-#SetStatus(voltmeter, {"interval": 10.0, "withgid": True})
-#
-#ConvergentConnect(noise, neuron, [epsc, ipsc], 1.0)
-#Connect(voltmeter, neuron)
-#Connect(neuron, spikedetector)
-#
-#Simulate(1000)
-#
-#vt.from_device(voltmeter)
-#plt.show()
+# This package generates 28 by 28 pixel images
+# It can also generate the input poisson spikes directly
+# Has to be tested whether this is faster than the Pynn Poisson sources
+import SEM_input
+
 
 
 seed = 764756387
@@ -36,7 +17,7 @@ tshow = 40.0 # ms
 tpaus = 10.0
 input_rate = 40.0 # Hz
 cell_params_lif = {
-               'V_th'  :-50.0,  # mV
+               'V_th'  :-55.0,  # mV
                'g_L'     : 10.0,  # ms 
                'tau_syn_ex' :  2.5,  # ms
                'tau_syn_in' :  5.0,  # ms
@@ -44,7 +25,7 @@ cell_params_lif = {
 
 # values taken from Naud et al. 2008
 cell_params_adex = {
-                'C_m'        :  0.2,  # nF
+                'C_m'        :  200.0,  # pF
                 'g_L'     : 10.0,  # ms
                 #'tau_refrac':  2.0,  # ms
                 'V_th'  :-50.0,  # mV
@@ -55,33 +36,44 @@ cell_params_adex = {
                 'b'         :100.0,  # pA
                 # V_T and delta_t default OK
                 'tau_w'     :120.0,  # ms
-                #'V_reset_'   :-46.0,  # mV
+                'V_reset'   :-46.0,  # mV
+                'tau_syn_ex': 1.0,
+                'tau_syn_in': 1.5,
                 'gsl_error_tol': 1e-8,
     }
 
-print GetDefaults('iaf_cond_exp')
-print GetDefaults('aeif_cond_exp')
-#SetDefaults('iaf_cond_exp',cell_params_lif)
-SetDefaults('aeif_cond_exp',{'gsl_error_tol':1e-7})
+SetDefaults('iaf_cond_exp',cell_params_lif)
+#SetDefaults('aeif_cond_exp',{'tau_syn_ex':1.5})
+#SetDefaults('aeif_cond_exp',{'tau_syn_in':1.5})
+SetDefaults('aeif_cond_exp',cell_params_adex)
+import pprint
+pp = pprint.PrettyPrinter()
+pp.pprint(GetDefaults('iaf_cond_exp'))
+pp.pprint(GetDefaults('aeif_cond_exp'))
+#pp.pprint(GetDefaults('aeif_cond_exp'))
+#sys.exit(0)
+
+SetStatus([0],{'overwrite_files':True})
 
 num = {}
-num['l0_exc_neurons'] = 64
+num['l0_exc_neurons'] = 20
 num['l0_exc_maxneighbors'] = 4
 num['l1_exc_neurons'] = 4
 num['l1_exc_maxneighbors'] = 4
 num['l0_l1_maxneighbors'] = 4
-num['l0_inh_neurons'] = 4
+num['l0_inh_neurons'] = 20
 num['l1_inh_neurons'] = 5
 num['inputs'] = SEM_input.SEM_input_config['num_inputs']
+print num['inputs']
 num['inputs_maxneighbors'] = 4
-num['steps'] = 100
+num['steps'] = 20
 num['steps_firing_rate_average'] = 10
 
 
 # input -> exc0
-w_inp_exc0_peak = 0.0002
-sigma_inp_exc0 = 0.2
-w_inp_exc0_max = 0.0045
+w_inp_exc0_peak = 0.2
+sigma_inp_exc0 = num['inputs']/3.0
+w_inp_exc0_max = 1.
 
 # exc0 -> exc1
 sigma_exc0_exc1 = 4.
@@ -90,11 +82,11 @@ w_exc0_exc1_max = 0.005
 
 # exc0 -> inh0
 p_exc0_inh0 = 1.0
-w_exc0_inh0 = 0.001
+w_exc0_inh0 = 10.0
 
 # exc0 -> exc0
-sigma_exc0_exc0 = 0.1
-w_exc0_exc0_max = 0.001
+sigma_exc0_exc0 = 0.5
+w_exc0_exc0_max = 0.5
 
 # exc1 -> exc1
 sigma_exc1_exc1 = 7.
@@ -106,12 +98,48 @@ w_exc1_inh1 = 0.02
 
 # inh0 -> exc0
 p_inh0_exc0 = 1.0
-w_inh0_exc0 = -0.01
+w_inh0_exc0 = -150.0
 
 # inh1 -> exc1
 p_inh1_exc1 = 1.0
 w_inh1_exc1 = 0.1
 
+
+def plot_neurons(meters,k,m,variables,title):
+    plt.figure()
+    plt.suptitle(title)
+    start = len(variables)*100+11
+    for var in variables:
+        plt.subplot(start)
+        plt.grid()
+        for i in range(k,m):
+            #meter = tp.GetElement(meters,[i,0])
+            events = GetStatus([meters[i]])[0]['events'] 
+            t = events['times']
+            plt.plot(t, events[var])
+            plt.ylabel(var)
+        start = start + 1
+
+def plot_weights(layer):
+    status = GetStatus(layer)[0]
+    weights = []
+    for i in range(status['topology']['columns']):
+        weights.extend(GetStatus(FindConnections(tp.GetElement(layer,[i,0])),'weight'))
+    plt.figure()
+    plt.hist(weights,bins=100)
+
+def plot_layer(layer):
+    tp.PlotLayer(layer, nodesize=50)
+    status = GetStatus(layer)[0]
+    extent = status['topology']['extent']
+    # beautify
+    plt.axis([extent[0]/(-2.0)-0.25, extent[0]/2.0+0.25, extent[1]/(-2.0)-0.25, extent[1]/2.0+0.25])
+    plt.axes().set_aspect('equal', 'box')
+    plt.axes().set_xticks([w-extent[0]/2.0 for w in range(int(extent[0])+1)])
+    plt.axes().set_yticks([w-extent[1]/2.0 for w in range(int(extent[1])+1)])
+    plt.grid(True)
+    plt.xlabel('%d Columns, Extent: %f' % (status['topology']['columns'],extent[0]))
+    plt.ylabel('%d Rows, Extent: %f' % (status['topology']['rows'],extent[1]))
 
 def setup_network():
 
@@ -120,19 +148,18 @@ def setup_network():
     l0_exc_population =  tp.CreateLayer ({
                                 'rows' : 1 ,
                                 'columns' : num['l0_exc_neurons'],
-                                #'extent' : [ float(num['l0_exc_neurons']), 1.  ],
+                                'extent' : [ float(num['l0_exc_neurons']), 1.  ],
                                 'elements' : 'aeif_cond_exp',
                                 'edge_wrap' : True } )
 
-    #tp.PlotLayer(l0_exc_population, nodesize=50)
 
     print "[ Creating inhibitory population ]"
     # 2) create inhibitory population
     l0_inh_population =  tp.CreateLayer ({
                                 'rows' : 1,
                                 'columns' : num['l0_inh_neurons'],
-                                #'extent' : [ float(num['l0_inh_neurons']), 1.  ] ,
-                                'elements' : 'iaf_cond_exp',
+                                'extent' : [ float(num['l0_inh_neurons']), 1.  ] ,
+                                'elements' : 'aeif_cond_exp',
                                 'edge_wrap' : True } )
 
 
@@ -147,12 +174,11 @@ def setup_network():
                         'upper_right':[0.5,0.5]
                     }
                 },
-                'kernel':p_exc0_inh0,
+                #'kernel':p_exc0_inh0,
                 'synapse_model': 'exc_inh'
             }
     tp.ConnectLayers(l0_exc_population,l0_inh_population,l0_exc_inh_dict)
-    #pynn.FixedProbabilityConnector(p_exc0_inh0,weights=w_exc0_inh0)
-    #l0_exc_inh_projection = pynn.Projection(l0_exc_population,l0_inh_population,inh_connector)
+
 
     print "[ Projecting excitatory -> excitatory population ]"
     # 4) exc -> exc
@@ -162,8 +188,8 @@ def setup_network():
                 'connection_type': 'convergent',
                 'mask': {
                     'rectangular': {
-                        'lower_left': [-0.5,-0.5],
-                        'upper_right':[0.5,0.5]
+                        'lower_left': [float(num['l0_exc_neurons']/(-2.0)),-0.5],
+                        'upper_right':[float(num['l0_exc_neurons']/(2.0)),0.5]
                     }
                 },
                 'synapse_model': 'exc_exc',
@@ -172,13 +198,7 @@ def setup_network():
                 }
             }
     tp.ConnectLayers(l0_exc_population,l0_exc_population,l0_exc_exc_dict)
-    #exc_connector = pynn.AllToAllConnector(weights=0.0)
-    #l0_exc_exc_projection = pynn.Projection(l0_exc_population,l0_exc_population,exc_connector)
-    #exc0_exc0_weights = l0_exc_exc_projection.getWeights()
 
-    #exc0_exc0_weights = connect_gauss(num['l0_exc_neurons'],num['l0_exc_neurons'],sigma_exc0_exc0,w_exc0_exc0_max,num['l0_exc_maxneighbors'],exc0_exc0_weights,True)
-
-    #l0_exc_exc_projection.setWeights(exc0_exc0_weights)
 
     print "[ Projecting inhibitory -> excitatory population ]"
     # 5) inh -> exc
@@ -186,12 +206,16 @@ def setup_network():
     l0_inh_exc_dict = {
                 'connection_type' : 'convergent',
                 'mask': {
-                    'rectangular': {
-                        'lower_left': [-0.5,-0.5],
-                        'upper_right':[0.5,0.5]
+                    'grid': {
+                        'rows': 1,
+                        'columns': num['l0_inh_neurons']-1,
+                    },
+                    'anchor': {
+                        'row': 0,
+                        'column': -1,
                     }
                 },
-                'kernel':p_inh0_exc0,
+                #'kernel':p_inh0_exc0,
                 'synapse_model': 'inh_exc'
             }
     tp.ConnectLayers(l0_inh_population,l0_exc_population,l0_inh_exc_dict)
@@ -199,30 +223,32 @@ def setup_network():
 
     print "[ Creating input population ]"
     # 6) create input population
-    CopyModel('poisson_generator','input_model',{'rate':50.0})
+    CopyModel('poisson_generator','input_model',{'rate':40.0})
     input_population =  tp.CreateLayer ({
                                 'rows' : 1 ,
                                 'columns' : num['inputs'],
-                                #'extent' : [ float(num['l0_exc_neurons']), 1.  ],
+                                'extent' : [ float(num['inputs']), 1.  ],
                                 'elements' : 'input_model',
                                 'edge_wrap' : True } )
 
     parrot_population =  tp.CreateLayer ({
                                 'rows' : 1 ,
                                 'columns' : num['inputs'],
-                                #'extent' : [ float(num['l0_exc_neurons']), 1.  ],
+                                'extent' : [ float(num['inputs']), 1.  ],
                                 'elements' : 'parrot_neuron',
                                 'edge_wrap' : True } )
+
 
     print "[ Projecting input -> excitatory population ]"
     # 7) input -> exc
     CopyModel('static_synapse','input_parrot', {'weight': 1.0})
-    #CopyModel('stdp_synapse','parrot_exc', {'weight':w_inp_exc0_peak,'alpha':0.012,'Wmax':w_inp_exc0_max})
-    CopyModel('static_synapse','parrot_exc', {'weight':w_inp_exc0_peak})
+    CopyModel('stdp_synapse','parrot_exc', {'weight':w_inp_exc0_peak,'alpha':0.5,'Wmax':w_inp_exc0_max})
+    CopyModel('static_synapse','parrot_inh', {'weight': w_inp_exc0_peak})
     input_parrot_dict = {
                 'connection_type' : 'convergent',
                 'mask': {
                     'rectangular': {
+                        # this is intended: only connect to one neuron!
                         'lower_left': [-0.5,-0.5],
                         'upper_right':[0.5,0.5]
                     }
@@ -232,69 +258,104 @@ def setup_network():
     parrot_exc_dict = {
                 'connection_type' : 'convergent',
                 'mask': {
-                    'rectangular': {
-                        'lower_left': [-0.05,-0.5],
-                        'upper_right':[0.05,0.5]
+                    'grid': {
+                        'rows': 1,
+                        'columns': num['inputs'],
+                    },
+                    'anchor': {
+                        'row': 0,
+                        'column': 0,
                     }
                 },
-                'weights': {'gaussian': {'p_center':1.,'sigma':sigma_inp_exc0}},
+                'kernel': {'gaussian': {'p_center':1.0,'sigma':sigma_inp_exc0}},
                 'synapse_model': 'parrot_exc'
             }
+
+    parrot_inh_dict = copy.copy(parrot_exc_dict)
+    parrot_inh_dict['synapse_model'] = 'parrot_inh'
+
     tp.ConnectLayers(input_population,parrot_population,input_parrot_dict)
     tp.ConnectLayers(parrot_population,l0_exc_population,parrot_exc_dict)
+    #tp.ConnectLayers(parrot_population,l0_inh_population,parrot_inh_dict)
 
-    #stdp_model = pynn.STDPMechanism(
-    #    timing_dependence=pynn.SpikePairRule(tau_plus=10.0, tau_minus=15.0),
-    #    weight_dependence=pynn.AdditiveWeightDependence(w_min=0, w_max=w_inp_exc0_max,
-    #    A_plus=0.012, A_minus=0.012)
-    #)
 
-    #connector = pynn.AllToAllConnector(weights=0.0)
-    #input_projection = pynn.Projection(
-    #        input_population,
-    #        l0_exc_population,
-    #        connector,
-    #        rng=rng,
-    #        synapse_dynamics=pynn.SynapseDynamics(slow=stdp_model)
-    #    )
-
-    #input_weights = input_projection.getWeights()
-
-    #print "[%d] Creating input projections" % node
-    #input_weights = connect_gauss(num['inputs'],num['l0_exc_neurons'],sigma_inp_exc0,w_inp_exc0_peak,num['inputs_maxneighbors'],input_weights,False)
-
-    #input_projection.setWeights(input_weights)
-
-    return l0_exc_population,l0_inh_population,input_population#,l1_projection
+    return l0_exc_population,l0_inh_population,input_population,parrot_population
 
 # CREATE
 populations = setup_network()
 # PRINT
 PrintNetwork(depth=2)
+#plot_layer(populations[2])
+#plot_layer(populations[3])
+#plt.figure() #ugly stack cleaning
 #print GetStatus(populations[0])
 #print GetStatus(populations[2])
-#tgts = GetStatus(FindConnections(tp.GetElement(populations[0],[0,0])))
-#print tgts
-#tgts = GetStatus(FindConnections(tp.GetElement(populations[0],[1,0])))
-#print tgts
+#for i in range(num['l0_exc_neurons']):
+#    print GetStatus(tp.GetElement(populations[0],[i,0]))
+#for i in range(num['l0_inh_neurons']):
+#    print GetStatus(tp.GetElement(populations[1],[i,0]))
+#    tgts = GetStatus(FindConnections(tp.GetElement(populations[1],[i,0])))
+#    print tgts
 #connPlot(populations[2],'input_model','aeif_cond_exp','input_exc','title')
+#plot_weights(populations[3])
 
 # CONNECT READOUTS
 #connect_readouts(l0_exc_population,l0_inh_population,input_population)
-voltmeters = Create('voltmeter',2)
-spikedetectors = Create('spike_detector',3)
+voltmeters = Create('multimeter',num['l0_exc_neurons']+num['l0_inh_neurons'],{'to_file':True,'record_from':['V_m','g_ex','g_in']})
+spikedetectors = Create('spike_detector',4,{'precise_times':True,'to_file':True})
 for n,pop in enumerate(populations):
+    SetStatus([spikedetectors[n]],{'label':str(n)})
     tgts = [nd for nd in GetLeaves(populations[n])[0]] 
     ConvergentConnect(tgts,[spikedetectors[n]])
-    # voltmeters only for non-inputs
-    if n < 2:
-        DivergentConnect([voltmeters[n]],[tgts[0]])
 
-Simulate(50)
-vt.from_device([voltmeters[0]])
-vt.from_device([voltmeters[1]])
+
+tgts = [nd for nd in GetLeaves(populations[0])[0]] 
+for i in range(num['l0_exc_neurons']):
+    DivergentConnect([voltmeters[i]],[tgts[i]])
+tgts = [nd for nd in GetLeaves(populations[1])[0]] 
+for i in range(num['l0_inh_neurons']):
+    DivergentConnect([voltmeters[i+4]],[tgts[i]])
+
+
+# GENERATE INPUT
+inputs = [nd for nd in GetLeaves(populations[2])[0]] 
+for i in range(num['steps']):
+
+    image = SEM_input.draw_image(SEM_input.SEM_input_config['centers'][i%4],SEM_input.SEM_input_config)
+
+    # prepare input population firing rates
+    for j in range(SEM_input.SEM_input_config['image_width']):
+        for k in range(SEM_input.SEM_input_config['image_width']):
+            SetStatus([inputs[2*j*SEM_input.SEM_input_config['image_width']+k*2]],{'rate': SEM_input.SEM_input_config['input_on_rate'] if (image[j][k] > 0.0) else SEM_input.SEM_input_config['input_off_rate']})
+            SetStatus([inputs[2*j*SEM_input.SEM_input_config['image_width']+k*2+1]],{'rate': SEM_input.SEM_input_config['input_on_rate'] if (image[j][k]<1.0) else SEM_input.SEM_input_config['input_off_rate']})
+
+    print "[Running simulation step %d]" % (i)
+    Simulate(tshow)
+
+    for j in range(SEM_input.SEM_input_config['image_width']):
+        for k in range(SEM_input.SEM_input_config['image_width']):
+            SetStatus([inputs[2*j*SEM_input.SEM_input_config['image_width']+k*2]],{'rate': 0.0})
+            SetStatus([inputs[2*j*SEM_input.SEM_input_config['image_width']+k*2+1]],{'rate': 0.0})
+
+    Simulate(tpaus)
+
+# RESULTS
+#plot_weights(populations[3])
+plot_neurons(voltmeters,0,3,['V_m','g_ex','g_in'],'exc')
+#vt.from_device([voltmeters[0]])
+#vt.from_device([voltmeters[1]])
+#vt.from_device([voltmeters[2]])
+#vt.from_device([voltmeters[3]])
+plot_neurons(voltmeters,4,7,['V_m','g_ex'],'inh')
+#vt.from_device([voltmeters[4]])
+#vt.from_device([voltmeters[5]])
+#vt.from_device([voltmeters[6]])
+#vt.from_device([voltmeters[7]])
+
 rp.from_device([spikedetectors[0]])
-#rp.from_device([spikedetectors[1]])
-rp.from_device([spikedetectors[2]])
+plt.title('excitatory')
+rp.from_device([spikedetectors[1]])
+plt.title('inhibitory')
+##rp.from_device([spikedetectors[2]])
 plt.show()
-#FindConnections(l0_exc_population)
+

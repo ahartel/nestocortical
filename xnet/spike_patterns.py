eaa579f3e4eca64ed9f6e9e9b011b21628e93ac1
Andreas Hatel <ahartel@kip.uni-heidelberg.de>
2014-09-06 21:34:56 +0200

    generalized spike_patterns.py

--- xnet/spike_patterns.py
+++ xnet/spike_patterns.py
@@ -1,17 +1,29 @@
-import sys
+import sys, argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
-num_repetitions = 100
+parser = argparse.ArgumentParser()
+parser.add_argument('filename')
+parser.add_argument('--start', help='linspace start value', type=float, dest='start')
+parser.add_argument('--stop', help='linspace stop value', type=float, dest='stop')
+parser.add_argument('--num', help='linspace num value', type=int, dest='num')
 
+args=parser.parse_args()
+
+# load spike data
 data = np.loadtxt(sys.argv[1],delimiter=',')
-bins = np.linspace(0,8*num_repetitions*100,8*num_repetitions+1)
+# create time bins every 100 ms
+# every second time bin codes for a ball passing by in a certain direction
+bins = np.linspace(args.start,args.stop,args.num)
+# this dict contains a list of neurons for every time bin
 times = {}
 
+# sort spikes by neurons
 spikes = [[] for n in range(48)]
 for sp in data:
     spikes[int(sp[0])].append(sp[1])
 
+# digitize spike times of every neuron and and the neuron's id to the times dict
 for n,neuron_spikes in enumerate(spikes):
     try:
         digitized = np.digitize(neuron_spikes, bins)
@@ -23,11 +35,8 @@ for n,neuron_spikes in enumerate(spikes):
     except ValueError:
         print "No spikes for neuron ",n
 
-for angle in range(1,17,2):
-	print 'angle ',angle/2
-	for i in range(angle,8*num_repetitions,16):
-		try:
-			print i,times[i]
-		except KeyError:
-			pass
+for bin,neurons in times.items():
+    print "Spikes in bin ",bin,":"
+    print neurons
+
 

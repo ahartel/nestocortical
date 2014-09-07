4435fe95d49bbfb18f66944fa0dbdadf7256ae53
Andreas Hartel <ahartel@gmx.de>
2014-09-07 16:01:12 +0200

    make spike_order_interval importable

--- evaluation_tools/spikes_order_interval.py
+++ evaluation_tools/spikes_order_interval.py
@@ -2,19 +2,26 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
-parser = argparse.ArgumentParser()
-parser.add_argument('filename')
-parser.add_argument('--start', help='beginning of interval', type=float, dest='start')
-parser.add_argument('--stop', help='ending of interval', type=float, dest='stop')
+def spikes_order_interval(filename,start,stop):
+    # load spike data
+    data = np.loadtxt(filename, delimiter=',')
 
-args=parser.parse_args()
+    ret = []
+    for sp in data:
+        if sp[1] > stop:
+            break
+        if sp[1] >= start:
+            ret.append(sp[0])
 
-# load spike data
-data = np.loadtxt(args.filename, delimiter=',')
+    return ret
 
-for sp in data:
-    if sp[1] > args.stop:
-        break
-    if sp[1] >= args.start:
-        print sp[0]
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    parser.add_argument('--start', help='beginning of interval', type=float, dest='start')
+    parser.add_argument('--stop', help='ending of interval', type=float, dest='stop')
 
+    args=parser.parse_args()
+
+    for sp in spikes_order_interval(args.filename, args.start, args.stop):
+        print sp

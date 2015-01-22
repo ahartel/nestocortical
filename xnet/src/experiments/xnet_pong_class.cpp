4d973de684c6d1082737c5b1f5c859a215bae56b
Andreas Hatel <ahartel@kip.uni-heidelberg.de>
2015-01-22 16:56:49 +0100

    added xnet_pong_class

--- xnet/src/experiments/xnet_pong_class.cpp
+++ xnet/src/experiments/xnet_pong_class.cpp
@@ -12,13 +12,29 @@
 
 using namespace std;
 
+template<typename T>
+size_t max(std::vector<T> const& input)
+{
+	T max_value = 0;
+	std::size_t max_pos = 0;
+	for (size_t i=0; i<input.size(); ++i)
+	{
+		if (input[i] > max_value)
+		{
+			max_value = input[i];
+			max_pos = i;
+		}
+	}
+	return max_pos;
+}
+
 int main(int argc, char* argv[])
 {
 	int court_width = 16;
 	int court_height = 16;
-	int num_output_neurons = 2;
+	int num_output_neurons = 16;
 	int num_intermediate_neurons = 16;
-	int num_dvs_addresses = court_height * 2;
+	int num_dvs_addresses = court_height + court_width;
 	Time_t dt = 10;
 	float timebase = 1.0e-4;
 	int	num_repetitions = atoi(argv[1]);
@@ -46,13 +62,12 @@ int main(int argc, char* argv[])
 		{15,0},
 		{50.0,30.0} // neuron parameters
 	);
-	auto control = theSimulation.create_population_fixed(num_output_neurons,{1000.0,50.,100,15});
+	auto control = theSimulation.create_population_fixed(court_height,{1000.0,50.,100,15});
 	//auto intermediate = theSimulation.create_population_fixed(
 	//	num_intermediate_neurons,
 	//	{3000.0,100.0,10,50} // neuron parameters
 	//);
-	auto output = theSimulation.create_population_fixed(num_output_neurons,{3000.0,200.0,10,1000});
-	auto go_on  = theSimulation.create_population_fixed(1,{5000.0,50.0,100,15});
+	auto output = theSimulation.create_population_fixed(num_output_neurons,{4000.0,100.0,10,1000});
 
 	//theSimulation.connect_all_to_all_normal(input,output0,
 	//										{1.0,0.2}, //wmin
@@ -63,11 +78,11 @@ int main(int argc, char* argv[])
 	//										{30.0,0.0} // ltp
 	//									);
 	theSimulation.connect_all_to_all_normal(input,output,
-											{100.0,20.0}, //wmin
-											{600.0,120.0}, //wmax
-											{400.0,80.0}, // winit
-											{50.0,10.0}, // ap
-											{25.0,5.0}, // am
+											{1.0,0.2}, //wmin
+											{1000.0,200.0}, //wmax
+											{800.0,160.0}, // winit
+											{100.0,20.0}, // ap
+											{50.0,10.0}, // am
 											{100.0,20.0} // ltp
 										);
 	//theSimulation.connect_all_to_all_normal(input,intermediate,
@@ -87,15 +102,8 @@ int main(int argc, char* argv[])
 	//										{100.0,10.0} // ltp
 	//									);
 
-	theSimulation.connect_all_to_all_identical(go_on,output,xnet::Weight(200.0,0.0,11000.0,0.0,0.0),15.0);
-	theSimulation.connect_all_to_all_identical(
-		xnet::Population(control.get(0),control.get(0)),
-		xnet::Population(output.get(0),output.get(0)),
-		xnet::Weight(3000.0,0.0,11000.0,0.0,0.0),15.0);
-	theSimulation.connect_all_to_all_identical(
-		xnet::Population(control.get(1),control.get(1)),
-		xnet::Population(output.get(1),output.get(1)),
-		xnet::Weight(3000.0,0.0,11000.0,0.0,0.0),15.0);
+	//theSimulation.connect_one_to_one_identical(control, output,
+	//	xnet::Weight(3000.0,0.0,11000.0,0.0,0.0),15.0);
 
 	//theSimulation.connect_all_to_all_wta(intermediate);
 	theSimulation.connect_all_to_all_wta(output);
@@ -112,32 +120,25 @@ int main(int argc, char* argv[])
 		for (Time_t t=0; t<court_width/std::get<0>(velocity)*2*10000; t+=dt)
 		{
 			auto output_spikes = theSimulation.get_new_spikes();
-			int up(0),down(0);
+			std::vector<int> guess(court_height);
 			// collect the spikes of the output neurons and count them
 			for (auto spike : output_spikes)
 			{
-				LOGGER("spike: " << std::get<0>(spike) << "," << std::get<1>(spike));
-				if (std::get<1>(spike) == output.get(0))
+				for (unsigned int n=output.begin(); n<output.end()+1;++n)
 				{
-					LOGGER("Found up spike");
-					up += 1;
-				}
-				else if (std::get<1>(spike) == output.get(1))
-				{
-					LOGGER("Found down spike");
-					down += 1;
+					if (std::get<1>(spike) == output.get(n))
+					{
+						guess[output.get(n)] += 1;
+					}
 				}
 			}
 			// advance the pong simulation by 1 ms
-			auto input_spikes = pong.advance(0.001,float(up-down)*paddle_speed);
+			auto input_spikes = pong.advance(0.001,max(guess));
 			// present the newly generated input data to the network
 			for (auto spike : input_spikes)
 			{
 				theSimulation.add_event(new xnet::pre_syn_event(time,std::get<1>(spike)));
 			}
-			// add an event for the go on population
-			if (time%100 == 0)
-				theSimulation.add_event(new xnet::pre_syn_event(time,go_on.get(0)));
 
 			time += dt;
 			theSimulation.run_until_empty();
@@ -151,4 +152,6 @@ int main(int argc, char* argv[])
 	theSimulation.print_pre_weights(output,filename_base+"/xnet_pong_weights_final_");
 
 }
+
 //# vim: set noexpandtab
+

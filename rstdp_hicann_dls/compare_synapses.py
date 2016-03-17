96d6a7da37189de0e7119591ae983a25ac4265db
Andreas Hartel <Andreas Hartel>
2016-03-17 10:48:25 +0100

    added unittest to compare_synapses.py

--- rstdp_hicann_dls/compare_synapses.py
+++ rstdp_hicann_dls/compare_synapses.py
@@ -1,20 +1,13 @@
-# import matplotlib as mpl
-# mpl.use('Agg')
+import matplotlib as mpl
+mpl.use('Agg')
 import nest
 import nest.voltage_trace
 import nest.raster_plot
 import numpy as np
 import matplotlib.pyplot as plt
+import unittest
 
 nest.Install("mymodule")
-
-t_sim = 2000.0  # how long we simulate
-r_inp = 300.0      # initial rate of the inhibitory population
-epsc = 40.0      # peak amplitude of excitatory synaptic currents
-
-ndict = {
-            "tau_m": 20.0,
-        }
 
 def get_weights(pop,synapse_model,value):
     conns = nest.GetConnections(pop, synapse_model=synapse_model)
@@ -30,82 +23,97 @@ def plot_weights(weights,title):
     plt.colorbar()
     return f
 
+class rstdp_synapse_test(unittest.TestCase):
+
+    t_sim = 2000.0  # how long we simulate
+    r_inp = 300.0      # initial rate of the inhibitory population
+    epsc = 40.0      # peak amplitude of excitatory synaptic currents
+
+    ndict = {
+                "tau_m": 20.0,
+            }
 
 
-def set_up_network():
-   # The actual neuron population
-    neuronpop = nest.Create("iaf_neuron", 1, params=ndict)
 
-    # The background noise that will be connected with plastic synapses
-    noise = nest.Create("poisson_generator", 1)
-    nest.SetStatus(noise, {"rate": r_inp})
-    # They need to be parroted to allow STDP
-    inputs = nest.Create("parrot_neuron",1)
-    nest.Connect(noise,inputs,{'rule':'one_to_one'})
+    def set_up_network(self):
+       # The actual neuron population
+        neuronpop = nest.Create("iaf_neuron", 1, params=self.ndict)
 
-    # Connect the parrots to the neuron via normal STDP synapse
-    conn_dict = {"rule": "all_to_all"}
-    syn_dict = {"model": "rstdp_synapse",
-                "mu_plus":0.0,
-                "mu_minus":0.0,
-                "alpha": 1.0,
-                "lambda": 0.001,
-                "weight": epsc}
-    nest.Connect(inputs, neuronpop, conn_dict, syn_dict)
+        # The background noise that will be connected with plastic synapses
+        noise = nest.Create("poisson_generator", 1)
+        nest.SetStatus(noise, {"rate": self.r_inp})
+        # They need to be parroted to allow STDP
+        inputs = nest.Create("parrot_neuron",1)
+        nest.Connect(noise,inputs,{'rule':'one_to_one'})
 
-    # Connect the parrots to the neuron via the RSTDP synapse
-    conn_dict = {"rule": "all_to_all"}
-    syn_dict = {"model": "stdp_synapse",
-                "mu_plus":0.0,
-                "mu_minus":0.0,
-                "alpha": 1.0,
-                "lambda": 0.001,
-                "weight": epsc}
-    nest.Connect(inputs, neuronpop, conn_dict, syn_dict)
+        # Connect the parrots to the neuron via normal STDP synapse
+        conn_dict = {"rule": "all_to_all"}
+        syn_dict = {"model": "rstdp_synapse",
+                    "mu_plus":0.0,
+                    "mu_minus":0.0,
+                    "alpha": 1.0,
+                    "lambda": 0.001,
+                    "weight": self.epsc}
+        nest.Connect(inputs, neuronpop, conn_dict, syn_dict)
+
+        # Connect the parrots to the neuron via the RSTDP synapse
+        conn_dict = {"rule": "all_to_all"}
+        syn_dict = {"model": "stdp_synapse",
+                    "mu_plus":0.0,
+                    "mu_minus":0.0,
+                    "alpha": 1.0,
+                    "lambda": 0.001,
+                    "weight": self.epsc}
+        nest.Connect(inputs, neuronpop, conn_dict, syn_dict)
 
 
-    voltmeter = nest.Create("voltmeter")
-    spikedetector = nest.Create("spike_detector")
+        voltmeter = nest.Create("voltmeter")
+        spikedetector = nest.Create("spike_detector")
 
-    nest.SetStatus(voltmeter, {"withgid": True, "withtime": True})
+        nest.SetStatus(voltmeter, {"withgid": True, "withtime": True})
 
-    nest.Connect(neuronpop, spikedetector)
-    nest.Connect(voltmeter, neuronpop)
+        nest.Connect(neuronpop, spikedetector)
+        nest.Connect(voltmeter, neuronpop)
 
-    return inputs,neuronpop,voltmeter,spikedetector
+        return inputs,neuronpop,voltmeter,spikedetector
+
+    def test_synapse(self):
+
+        inputs, neuronpop, voltmeter, spikedetector = self.set_up_network()
+
+        weights_before = {}
+        weights_after = {}
+
+        weights_before['stdp'] = get_weights(inputs,'stdp_synapse','weight')
+        weights_before['rstdp'] = get_weights(inputs,'rstdp_synapse',
+                                              'accumulation')
+                                              # 'weight')
+
+        # f = plot_weights(weights_before['stdp'],'stdp - before')
+        # f = plot_weights(weights_before['rstdp'],'rstdp - before')
+        # f.savefig('plots/'+run_str+'_before_weights.png')
+        print weights_before
+
+        nest.Simulate(self.t_sim)
+
+        weights_after['stdp'] = get_weights(inputs,'stdp_synapse','weight')
+        weights_after['rstdp'] = get_weights(inputs,'rstdp_synapse',
+                                             # 'weight')
+                                             'accumulation')
+        self.assertEqual(weights_after['stdp'],weights_after['rstdp'])
+
+        # f = plot_weights(weights_after['stdp'],'stdp -after')
+        # f = plot_weights(weights_after['rstdp'],'rstdp -after')
+        # f.savefig('plots/'+run_str+'_weight_diff.png')
+        # plt.figure()
+        # print weights_after
+        self.assertEqual(get_weights(inputs,'rstdp_synapse','weight'),
+                         self.epsc)
+
+        # nest.voltage_trace.from_device(voltmeter)
+        # nest.raster_plot.from_device(spikedetector, hist=True)
 
 if __name__ == '__main__':
+    unittest.main()
 
-    inputs, neuronpop, voltmeter, spikedetector = set_up_network()
-
-    weights_before = {}
-    weights_after = {}
-
-    weights_before['stdp'] = get_weights(inputs,'stdp_synapse','weight')
-    weights_before['rstdp'] = get_weights(inputs,'rstdp_synapse',
-                                          'accumulation')
-                                          # 'weight')
-
-    # f = plot_weights(weights_before['stdp'],'stdp - before')
-    # f = plot_weights(weights_before['rstdp'],'rstdp - before')
-    # f.savefig('plots/'+run_str+'_before_weights.png')
-    print weights_before
-
-    nest.Simulate(t_sim)
-
-    weights_after['stdp'] = get_weights(inputs,'stdp_synapse','weight')
-    weights_after['rstdp'] = get_weights(inputs,'rstdp_synapse',
-                                         # 'weight')
-                                         'accumulation')
-
-    # f = plot_weights(weights_after['stdp'],'stdp -after')
-    # f = plot_weights(weights_after['rstdp'],'rstdp -after')
-    # f.savefig('plots/'+run_str+'_weight_diff.png')
-    # plt.figure()
-    print weights_after
-
-
-    nest.voltage_trace.from_device(voltmeter)
-    nest.raster_plot.from_device(spikedetector, hist=True)
-    plt.show()
 

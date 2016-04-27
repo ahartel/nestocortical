19051d090a6bd5305a7f5979cf44f990da5f1d0f
Andreas Hartel <Andreas Hartel>
2016-04-27 15:20:07 +0200

    version I came up with after discussions with Paul

--- rstdp_hicann_dls/rstdp.py
+++ rstdp_hicann_dls/rstdp.py
@@ -1,40 +1,56 @@
 """RSTDP experiment for Hicann-DLS. Here, it is implemented with Nest."""
 # import sys
-import nest
-import math
-import copy
 import numpy as np
+import nest
+# import math
+import copy
+import random
 import pickle as pkl
 import nest.raster_plot
 import nest.voltage_trace
+import matplotlib.pyplot as plt
 from spike_train import vp_metric_other
 
 nest.Install("mymodule")
 
+SEED = 42
+
 TSIM = 1000.0  # how long we simulate
-r_inp = 40.0
-r_inp_bg = 100.0
-epsc_bg = 15.0
-ipsc_bg = -10.0
+R_INP = 10.0
+R_INP_BG = 100.0
+EPSC_BG = 5.0
+IPSC_BG = -5.0
 EPSC = 20.0
+TARGET_EPSP = 80.0
 
 # COST = 10.0
-COST = 0.20
+COST = 0.10
 
-NUM_RUNS = 2000
-NUM_NEURONS = 3
-PLOT_WEIGHTS = False
-NUM_BG_INH = 1
-NUM_BG_EXC = 1
+NUM_RUNS = 1000
+NUM_NEURONS = 1
+NUM_BG_INH = 14
+NUM_BG_EXC = 14
 NUM_BG = NUM_BG_INH + NUM_BG_EXC
 
-figures = []
-weight_diff_storage = np.zeros((NUM_RUNS, (32-NUM_BG)*NUM_NEURONS, 1))
-rates = np.zeros((NUM_NEURONS, NUM_RUNS))
 
 NEURON_DICT = {
-    "tau_m": 20.0,
+    "tau_m": 30.0,
+    "V_th": -68.0
     }
+
+SYN_DICT = {"model": "rstdp_synapse",
+            "mu_plus":0.0,
+            "mu_minus":0.0,
+            "alpha": 1.0,
+            "lambda": 0.0001,
+            "lambda": 0.001,
+            "tau_plus": 20.0,
+            "Wmax": 200.0,
+            # "tau_minus": 20.0,
+            # "weight": {'distribution': 'normal_clipped', 'low': 0.5,
+            #            'mu': EPSC/1.0, 'sigma': EPSC/4.0}}
+            "weight": {'distribution': 'uniform', 'low': EPSC*0.7,
+                       'high': EPSC*1.3}}
 
 
 def get_weights(pop):
@@ -51,109 +67,129 @@ def set_weights(pop, weights):
     conns = nest.GetConnections(pop, synapse_model="rstdp_synapse")
     for conn, wgt in zip(conns, weights):
         nest.SetStatus([conn], {"weight": wgt[0]})
-    # check if it worked
-    # plot_weights(np.array(nest.GetStatus(conns, ["weight"])))
 
-# def plot_weights(weights):
-    # reshaped = np.reshape(weights,((32-NUM_BG),NUM_NEURONS))
-    # f = plt.figure()
-    # plt.imshow(reshaped,interpolation='none')
-    # plt.colorbar()
-    # return f
 
-def get_spiketrains(spikedetector):
+def get_spiketrains(nrnpop, spikedetector):
     events = nest.GetStatus(spikedetector, ["events"])[0][0]
     out = [[] for nrn in range(NUM_NEURONS)]
     for nrn, time in zip(events['senders'], events['times']):
-        out[nrn-1].append(time)
+        out[nrnpop.index(nrn)].append(time)
 
     return out
 
-def calculate_reward(run, spikedetector, target):
-    global rates
+def calculate_reward(nrnpop, spikedetector, target):
     reward = 0.0
+    rates = np.zeros(NUM_NEURONS)
 
-    out = get_spiketrains(spikedetector)
+    out = get_spiketrains(nrnpop, spikedetector)
 
     for neuron in range(NUM_NEURONS):
         num = len(out[neuron])
-        print "Measured:",out[neuron]
-        print " ",num
-        print "Target:",target[neuron]
-        print " ",len(target[neuron])
+        # print "Measured:", out[neuron]
+        # print " ", num
+        # print "Target:", target[neuron]
+        # print " ", len(target[neuron])
 
         total_spikes = num + len(target[neuron])
         distance = vp_metric_other(target[neuron], out[neuron], COST)
-        print "Distance:",distance
+        print "Distance:", distance
 
         add_reward = 1.0 - distance/total_spikes
-        print "Reward:",add_reward
+        print "Reward:", add_reward
         reward += add_reward
 
-        rates[neuron][run] = num
+        rates[neuron] = num
 
     reward /= float(NUM_NEURONS)
 
-    return reward, out
+    return reward, out, rates
 
-def apply_reward(pop, weights_before, weights_after, reward, mean_reward):
+def apply_reward(pop, wgt_before, wgt_after, reward, mean_reward):
     """Set the new weights of pop using set_weights. The weights are calculated
     as::
-        new_weights = (weights_after-weights_before)*success + weights_before
+        new_weights = (wgt_after-wgt_before)*success + wgt_before
     """
 
-    success = reward-mean_reward
+    success_signal = reward-mean_reward
 
-    new_weights = (weights_after-weights_before)*success*10.0 + weights_before
+    new_weights = (wgt_after-wgt_before)*success_signal*10.0 + wgt_before
 
     set_weights(pop, new_weights)
 
-    return success
+    return success_signal
 
 
-def set_up_network(epsc):
+def set_up_network_poisson_input():
+    """Configure neuron populations.
+    ========== ================= =================
+    name       number of neurons type
+    ========== ================= =================
+    noise      32-NUM_BG         poisson_generator
+    inputs     32-NUM_BG         parrot_neuron
+    ========== ================= =================
+    """
+    # The actual random input that will be connected with plastic synapses
+    noise = nest.Create("poisson_generator", 32-NUM_BG)
+    nest.SetStatus(noise, {"rate": R_INP})
+    # They need to be parroted to allow STDP
+    inputs = nest.Create("parrot_neuron", 32-NUM_BG)
+    nest.Connect(noise, inputs, {'rule':'one_to_one'})
+
+    return set_up_network_trunk(inputs)
+
+def set_up_network_serial_input():
+    """Configure neuron populations.
+    ========== ================= =================
+    name       number of neurons type
+    ========== ================= =================
+    inputs     32-NUM_BG         spike_generator
+    ========== ================= =================
+    """
+    source = nest.Create('spike_generator', 32-NUM_BG)
+    for inpt in range(32-NUM_BG):
+        nest.SetStatus([source[inpt]],
+                       {'spike_times':[1.0 + 30.0 * float(inpt),]})
+                                       # 2.0 + 30.0 * float(inpt),
+                                       # 3.0 + 30.0 * float(inpt),
+                                       # 4.0 + 30.0 * float(inpt),
+                                       # 5.0 + 30.0 * float(inpt)]})
+
+    # They need to be parroted to allow STDP
+    inputs = nest.Create("parrot_neuron", 32-NUM_BG)
+    nest.Connect(source, inputs, {'rule':'one_to_one'})
+
+    return set_up_network_trunk(inputs)
+
+def set_up_network_trunk(inputs):
     """Configure neuron populations.
     ========== ================= =================
     name       number of neurons type
     ========== ================= =================
     neuronpop  NUM_NEURONS       iaf_neuron
-    noise      32-NUM_BG         poisson_generator
-    inputs     32-NUM_BG         parrot_neuron
     background NUM_BG            poisson_generator
     ========== ================= =================
     """
+
     # The actual neuron population
     neuronpop = nest.Create("iaf_neuron", NUM_NEURONS, params=NEURON_DICT)
     for neuron in neuronpop:
-        nest.SetStatus([neuron], {"tau_m": np.random.uniform(15.0, 25.0)})
-
-    # The background noise that will be connected with plastic synapses
-    noise = nest.Create("poisson_generator", 32-NUM_BG)
-    nest.SetStatus(noise, {"rate": r_inp})
-    # They need to be parroted to allow STDP
-    inputs = nest.Create("parrot_neuron", 32-NUM_BG)
-    nest.Connect(noise, inputs, {'rule':'one_to_one'})
+        # print nest.GetStatus([neuron])
+        nest.SetStatus([neuron], {"tau_m": np.random.uniform(25.0, 35.0)})
 
     # Connect the parrots to the neurons under test
     conn_dict = {"rule": "all_to_all"}
-    syn_dict = {"model": "rstdp_synapse",
-                "mu_plus":0.0,
-                "mu_minus":0.0,
-                "alpha": 1.0,
-                "lambda": 0.0001,
-                "weight": epsc}
-    nest.Connect(inputs, neuronpop, conn_dict, syn_dict)
+    nest.Connect(inputs, neuronpop, conn_dict, SYN_DICT)
 
     # Apply some non-changing background
     if NUM_BG > 0:
         bg_syn_dict_exc = {"model": "static_synapse",
-                           "weight": epsc_bg}
+                           "weight": EPSC_BG}
         bg_syn_dict_inh = {"model": "static_synapse",
-                           "weight": ipsc_bg}
+                           "weight": IPSC_BG}
         background_inh = nest.Create("poisson_generator", NUM_BG_INH)
         background_exc = nest.Create("poisson_generator", NUM_BG_EXC)
-        nest.SetStatus(background_inh, {"rate": r_inp_bg})
-        nest.SetStatus(background_exc, {"rate": r_inp_bg})
+        nest.SetStatus(background_inh, {"rate": R_INP_BG})
+        nest.SetStatus(background_exc, {"rate": R_INP_BG})
         nest.Connect(background_exc, neuronpop, conn_dict, bg_syn_dict_exc)
         nest.Connect(background_inh, neuronpop, conn_dict, bg_syn_dict_inh)
 
@@ -167,35 +203,61 @@ def set_up_network(epsc):
 
     return inputs, neuronpop, voltmeter, spikedetector
 
-def generate_target_spiketrain(pop, spikedetector, epsc):
-    old_weights = get_weights(pop)
+def generate_target_spiketrain(inputpop, nrnpop, spikedetector):
+    """Apply a fixed weight pattern to the input population and simulate for
+    TSIM to get a target output spike train."""
+    old_weights = get_weights(inputpop)
     weights = copy.copy(old_weights)
     for row in range(len(weights)):
-        weights[row] = 5*math.sin(float(row)/10.0) + epsc
+        # weights[row] = 5*math.sin(float(row)/10.0) + EPSC
+        # weights[row] = random.randint(0, 1) * TARGET_EPSP
+        weights[row] = 0
+    weights[int((32-NUM_BG)/2)] = TARGET_EPSP
     print weights
-    set_weights(pop, weights)
+    set_weights(inputpop, weights)
     nest.Simulate(TSIM)
-    target_trains = get_spiketrains(spikedetector)
+    target_trains = get_spiketrains(nrnpop, spikedetector)
     print target_trains
 
     return target_trains
 
-if __name__ == '__main__':
+def reset_kernel():
+    old_seed = nest.GetStatus([0])[0]['rng_seeds'][0]
+    nest.ResetKernel()
+    nest.SetStatus([0], {'rng_seeds': (old_seed + 1,)})
+    # print old_seed,nest.GetStatus([0])
+
+
+def main_function():
+    """This function exists only to tell pylint that the following variables
+    are variables and not functions."""
     rewards = []
-    mean_R = 0.5
+    mean_reward = 0.5
     successes = []
     mean_rewards = []
     spiketrains = []
-
-    inputs, neuronpop, voltmeter, spikedetector = set_up_network(EPSC)
+    weight_diff_storage = np.zeros((NUM_RUNS, (32-NUM_BG)*NUM_NEURONS, 1))
+    rates = np.zeros((NUM_RUNS, NUM_NEURONS))
 
     target = [[] for nrn in range(NUM_NEURONS)]
-    is_empty = lambda x : not x
-    while any(map(is_empty, target)):
-        target = generate_target_spiketrain(inputs, spikedetector, EPSC)
+    is_empty = lambda x: not x
+    while any([is_empty(tgt) for tgt in target]):
+        reset_kernel()
+        # inputs, neuronpop, voltmeter, spikedetector = \
+                                                # set_up_network_serial_input()
+        inputs, neuronpop, voltmeter, spikedetector = \
+                                                set_up_network_poisson_input()
+        target = generate_target_spiketrain(inputs, neuronpop, spikedetector)
+        nest.voltage_trace.from_device([voltmeter[0]])
+        plt.show()
 
-    nest.ResetKernel()
-    inputs, neuronpop, voltmeter, spikedetector = set_up_network(EPSC)
+    target_weights = get_weights(inputs)
+
+    reset_kernel()
+    # inputs, neuronpop, voltmeter, spikedetector = set_up_network_serial_input()
+    inputs, neuronpop, voltmeter, spikedetector = set_up_network_poisson_input()
+
+    initial_weights = get_weights(inputs)
 
     for run in range(NUM_RUNS):
         run_str = "%03d" % (run,)
@@ -204,50 +266,50 @@ if __name__ == '__main__':
         weights_before = get_weights(inputs)
         # print weights_before
 
-        # if PLOT_WEIGHTS:
-            # f = plot_weights(weights_before)
-            # if SAVEFIG:
-                # f.savefig('plots/'+run_str+'_before_weights.png')
-
         nest.Simulate(TSIM)
 
-        # lines = nest.voltage_trace.from_device([voltmeter[1]])
+        # lines = nest.voltage_trace.from_device([voltmeter[0]])
         # lines[0][0].figure.savefig('plots/'+run_str+'_voltage_trace.png')
+        # plt.show()
         # f = nest.raster_plot.from_device(spikedetector, hist=True)
         # f[0].figure.savefig('plots/'+run_str+'_raster_plot.png')
 
-        reward, spiketrains_run = calculate_reward(run, spikedetector, target)
+        reward, spiketrains_run, rates[run] = calculate_reward(neuronpop,
+                                                               spikedetector,
+                                                               target)
         spiketrains.append(spiketrains_run)
 
         weights_after = get_weights(inputs)
         weight_diff_storage[run] = weights_after-weights_before
 
-        nest.ResetKernel()
-        inputs, neuronpop, voltmeter, spikedetector = set_up_network(EPSC)
+        reset_kernel()
+        # inputs, neuronpop, voltmeter, spikedetector = set_up_network_serial_input()
+        inputs, neuronpop, voltmeter, spikedetector = \
+                                                set_up_network_poisson_input()
 
         success = apply_reward(inputs, weights_before, weights_after, reward,
-                               mean_R)
+                               mean_reward)
 
         rewards.append(reward)
         successes.append(success)
-        mean_R = mean_R + (reward-mean_R)/5.0
-        mean_rewards.append(mean_R)
-
-        # if PLOT_WEIGHTS:
-            # f = plot_weights((weights_after-weights_before)*reward)
-            # if SAVEFIG:
-                # f.savefig('plots/'+run_str+'_weight_diff.png')
-            # plt.figure()
+        mean_reward = mean_reward + (reward-mean_reward)/5.0
+        mean_rewards.append(mean_reward)
 
     pkl.dump(rates, open("data/firing_rates.pkl", 'w'))
     pkl.dump(spiketrains, open("data/spiketrains.pkl", 'w'))
     pkl.dump(target, open("data/target_spiketrains.pkl", 'w'))
 
+    pkl.dump(target_weights, open("data/target_weights.pkl", 'w'))
+    pkl.dump(initial_weights, open("data/initial_weights.pkl", 'w'))
     pkl.dump(get_weights(inputs), open("data/final_weights.pkl", 'w'))
-    # f = plot_weights(get_weights(inputs))
-    # if SAVEFIG:
-        # f.savefig("plots/final_weights.png")
 
     pkl.dump((rewards, successes, mean_rewards), open('data/rewards.pkl', 'w'))
     pkl.dump(weight_diff_storage, open('data/weight_diffs.pkl', 'w'))
+
+if __name__ == '__main__':
+    random.seed(1)
+    np.random.seed(seed=1)
+    nest.SetStatus([0], {'rng_seeds': (SEED,)})
+    print nest.GetStatus([0])
+    main_function()
 
